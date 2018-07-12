db4fc0bcb1f7070df8dbd4bc00e61d81607e9dbc
xyyjk <xyyjk@126.com>
2018-07-12 14:24:06 +0800

    ‘’

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ import main from './js/main';
 main();
 
 document.getElementById('root').innerHTML = 'hello word';
+
+var a = { a: 1};
+var b= {b: 2}
+
+var c = {c: 3, ...a, ...b }
+console.log(c);
+
+
+async function abc(url, args) {
+  const data = await 1;
+
+  return data;
+}
+
+abc().then(data => console.log(data));
